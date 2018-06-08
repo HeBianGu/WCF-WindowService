15c8ef9ed2a25a16f88c9cf60ad8a6e3596e6d39
lhj <navylee0210@163.com>
2018-06-08 09:28:44 +0800

    11

---
+++ Solution/HeBianGu.Test.WCF.OracleConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using HeBianGu.Domain.WCF.DataManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeBianGu.Test.WCF.OracleConsoleApp
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                try
+                {
+                    var collection = DataService.Instance.GetAll();
+
+                    foreach (var item in collection)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                Console.Read();
+            }
+       
+    }
+}
